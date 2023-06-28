60611f3d163833d53e9b20ac53131996d03c69a6
Matias gomez <mrmatiasgomez@gmail.com>
2023-06-28 23:02:34 +0200

    Modif card products

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -3,6 +3,9 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useState } from 'react';
 import axios from 'axios';
+import { ButtonGroup, ListGroup } from 'react-bootstrap';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function CardProducts() {
 
@@ -29,14 +32,26 @@ export default function CardProducts() {
             <div className='row justify-content-center'>
                 {products.map((produit) => (
                     <Card key={produit._id} className='col-3 me-2'>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src={produit.photo} />
                         <Card.Body>
-                            <Card.Title>{produit.name}</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary">réserver</Button>
+                            <Card.Title className='fw-bold mt-2'>{produit.name}</Card.Title>
+
+                            <ListGroup className='list-group-flush text-start'>
+                                <ListGroup.Item>Poids : {produit.weight}</ListGroup.Item>
+                                <ListGroup.Item>Quantité : {produit.quantity}</ListGroup.Item>
+                            </ListGroup>
+
+                            <div className='d-flex justify-content-between mt-4'>
+                                <div className='fw-bold fs-5 align-self-center'>{produit.price}$</div>
+                                <div>
+                                    <ButtonGroup className='me-2' aria-label="Basic example">
+                                        <Button className='border-secondary' variant="transparent">-</Button>
+                                        <Button className='border-secondary' variant="transparent">1</Button>
+                                        <Button className='border-secondary' variant="transparent">+</Button>
+                                    </ButtonGroup>
+                                    <Button className='text-end' variant="primary"><FontAwesomeIcon icon={faCartPlus} /></Button>
+                                </div>
+                            </div>
                         </Card.Body>
                     </Card>
                 ))}
