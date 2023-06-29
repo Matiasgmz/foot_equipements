5da72c3f5405a25ca4c5100149bdfd6a720c495f
Matias gomez <mrmatiasgomez@gmail.com>
2023-06-29 17:51:57 +0200

    Modif de la barre de recherche users et ajout du button deconnexion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
-import { Nav } from 'react-bootstrap';
+import { Button, Dropdown, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
@@ -23,9 +23,19 @@ export default function Header() {
                         <Link className='text-decoration-none text-light' to='/gestionUtilisateur'>Gérer les utilisateurs</Link>
                     </Nav>
                     <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>
-                            <Link to='/panier' className='bg-transparent border-0'><FontAwesomeIcon className='fs-5 me-3' icon={faBasketShopping} /></Link>
-                            <Link to='/connexion' className='bg-transparent border-0'><FontAwesomeIcon className='fs-5 p-0 m-0' icon={faUser} /></Link>
+                        <Navbar.Text className='d-flex p-0 m-0'>
+                            <Link to='/panier' className='bg-transparent border-0 align-self-center'><FontAwesomeIcon className='fs-5 me-3' icon={faBasketShopping} /></Link>
+                            <Dropdown>
+                                <Dropdown.Toggle variant="transparant" id="dropdown-basic">
+                                    <FontAwesomeIcon className='fs-5 p-0 m-0 text-light' icon={faUser} />
+                                </Dropdown.Toggle>
+
+                                <Dropdown.Menu>
+                                    <Link to='/connexion' className='ms-2 bg-transparent border-0 text-decoration-none'>Connexion</Link>
+                                    <br />
+                                    <Link className='ms-2 bg-transparent border-0 text-decoration-none'>Déconnexion</Link>
+                                </Dropdown.Menu>
+                            </Dropdown>
                         </Navbar.Text>
                     </Navbar.Collapse>
                 </Container>
