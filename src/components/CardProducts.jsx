7b3709024904a529d2f06d500319f5866afcf5d0
Matias gomez <mrmatiasgomez@gmail.com>
2023-06-29 16:35:48 +0200

    Ajout de la barre de recherche article

--- src/components/CardProducts.jsx
+++ src/components/CardProducts.jsx
@@ -3,13 +3,14 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { useState } from 'react';
 import axios from 'axios';
-import { ButtonGroup, ListGroup } from 'react-bootstrap';
+import { ButtonGroup, Form, ListGroup } from 'react-bootstrap';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function CardProducts() {
 
     const [products, setProducts] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         fetchData();
@@ -31,24 +32,28 @@ export default function CardProducts() {
 
     const incrementCount = (index) => {
         const updatedCounters = [...counters];
-        updatedCounters[index] = (updatedCounters[index] || 0) + 1; 
+        updatedCounters[index] = (updatedCounters[index] || 0) + 1;
         setCounters(updatedCounters);
     };
 
     const decrementCount = (index) => {
         if (counters[index] > 0) {
             const updatedCounters = [...counters];
-            updatedCounters[index] = updatedCounters[index] - 1; 
+            updatedCounters[index] = updatedCounters[index] - 1;
             setCounters(updatedCounters);
         }
     };
 
     const addToCart = (produit, index) => {
-        const cartItems = JSON.parse(localStorage.getItem('cartItems')) || []; 
+        const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
         const newItem = { produit, count: counters[index] || 0 };
-        cartItems.push(newItem); 
-        localStorage.setItem('cartItems', JSON.stringify(cartItems)); 
+        cartItems.push(newItem);
+        localStorage.setItem('cartItems', JSON.stringify(cartItems));
     };
+
+    const filteredProducts = products.filter((product) => {
+        return product.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
 
 
     return (
@@ -56,10 +61,18 @@ export default function CardProducts() {
             <div className='row justify-content-center'>
                 <h1 className='my-5 '>Articles</h1>
                 <hr className='col-10' />
-                {products.map((produit, index) => (
+                <div className='col-10'>
+                    <Form.Control
+                        type="text"
+                        placeholder="Rechercher un produit"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                </div>
+                {filteredProducts.map((produit, index) => (
                     <Card key={produit._id} className='col-xxl-2 col-md-3 col-sm-4 me-2 mt-4'>
-                        <Card.Img variant="top" src={produit.photo} style={{width: '100%', height: '350px', objectFit: 'cover'}} />
-                        
+                        <Card.Img variant="top" src={produit.photo} style={{ width: '100%', height: '350px', objectFit: 'cover' }} />
+
                         <Card.Body>
                             <Card.Title className='fw-bold mt-2'>{produit.name}</Card.Title>
 
